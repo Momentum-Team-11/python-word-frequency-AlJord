548ca8a1a6cfecd8f2926df21cbbd5988ce403fb
Alex Jordan <AlJord91@gmail.com>
2022-02-22 21:17:18 -0500

    words are split, punct removed!!

--- word_frequency.py
+++ word_frequency.py
@@ -1,13 +1,28 @@
+from string import punctuation
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
+PUNCTUATION =  string.punctuation 
+
 
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    with open(file) as file:
+        text_string = file.read()
+        words = text_string.lower()
+        stripped_text = " "
+        for char in words:
+            if not char in PUNCTUATION:
+                stripped_text += char
+        line_break = stripped_text.replace('\n',' ').replace('—', ' ')
+        squeaky_clean = line_break.split(" ")
+        print(squeaky_clean)
+
 
 
 if __name__ == "__main__":
@@ -25,3 +40,21 @@ if __name__ == "__main__":
     else:
         print(f"{file} does not exist!")
         exit(1)
+
+
+
+#remove punctuation
+#list_of_words.split(" ")
+
+
+#everything into lowercase
+
+
+
+#remove requently used words 'and' etc --create dictionary
+# and in list_of_words  then move to unincluded_list
+
+
+#keep count of how often each word is used (will be using f-strings)
+# for words in list_of_words
+#(method dictorary- .keys())
